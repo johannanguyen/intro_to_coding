7e0b8c956257c2bf8211579b340ef3066f980b2b
johannanguyen <johannatnguyen@gmail.com>
2026-02-20 10:02:08 -0700

    pushing initial file

---
+++ week1/assignment.tsx
@@ -0,0 +1,31 @@
+/*
+    Commands to install:
+    node --version
+    npm install -g tsx
+
+    Command to run:
+    tsx assignment.tsx
+*/
+
+const MAX_LIVES: number = 3;
+let playerName: string = "Jordan";
+let currentLives: number = MAX_LIVES;
+let isGameOver: boolean = false;
+
+// Print the player's name
+console.log(playerName);
+
+// Print current amount of lives
+console.log(currentLives)
+
+// Decrease lives by 1;
+currentLives = currentLives - 1;
+
+//Decrease lives by 1;
+currentLives = currentLives - 1;
+
+// What is the current amount of lives?
+//console.log(currentLives);
+
+// Is the game over?
+//console.log(isGameOver);

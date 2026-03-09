34690c22e000fc2f5649943f7a62fe73dc8f4fde
johannanguyen <johannatnguyen@gmail.com>
2026-03-09 07:59:17 -0700

    loop file from slideshow

---
+++ week3/loops.tsx
@@ -0,0 +1,26 @@
+// For Loop
+for (let i = 0; i < 100; i++) {
+  console.log(`Hello ${i} This is a for loop`);
+}
+
+// While loop
+let count = 0;
+
+while (count < 5) {
+  console.log("count is: " + count + "This is a while loop");
+  count++;
+}
+
+// Infinite loop
+// while (2 > 1) {
+//   console.log("This is an infinite loop");
+// }
+
+// Loop with if statements
+for (let i = 0; i < 10; i++) {
+  if (i % 2 === 0) {
+    console.log(`${i} is even`);
+  } else {
+    console.log(`${i} is odd`);
+  }
+}
